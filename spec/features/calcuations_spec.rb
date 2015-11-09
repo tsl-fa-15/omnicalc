f21fa83f1519d371bc94f234bbfc9a540af90cf8
Arjun Venkataswamy <avenkat2@gmail.com>
2015-11-09 15:37:44 -0600

    refactors feature tests

--- spec/features/calcuations_spec.rb
+++ spec/features/calcuations_spec.rb
@@ -10,15 +10,33 @@ RSpec.describe "calcuations", :type => :feature do
     expect(page).to have_css('form[action="/descriptive_statistics/results"]')
   end
 
-  it 'displays submitted text in word count results' do
-    visit "/word_count/new"
-    fill_in "Text", :with => "Hello world"
-    fill_in "user_word", :with => "world"
-    click_button "Submit"
+  describe 'word count feature', :type => :feature do
 
-    expect(page).to have_content('Hello world')
-  end
+    before(:each) do
+      visit "/word_count/new"
+      fill_in "Text", :with => "Hello world"
+      fill_in "user_word", :with => "world"
+      click_button "Submit"
+    end
 
-  it "displays the submitted user word in word count results" do
+    it 'displays submitted text in word count results' do
+      expect(page).to have_content('Hello world')
+    end
+
+    it "displays the submitted user word in word count results" do
+      expect(page).to have_content('Occurrences of world')
+    end
+
+    it 'displays the correct word count in word count results' do
+      expect(page).to have_content('2')
+    end
+
+    it 'displays the correct character count with spaces in word count results' do
+      expect(page).to have_content('11')
+    end
+
+    it 'displays the correct character count without spaces in word count results' do
+      expect(page).to have_content('10')
+    end
   end
 end
